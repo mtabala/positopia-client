4f27ff782102e4cc1d7e8bd7ee012c595f294846
Masha <mariia.tabala@gmail.com>
2023-04-07 13:04:16 -0700

    add user profile jsx structure, fix the styling for header and hero section, add box shadow for video

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import "../UserProfile/UserProfile.scss"
+import heart from "../../assets/icons/heart.svg"
 
 function UserProfile({ userImage, userName, userDescription }) {
     return (
         <>
-            <div className="user__profile">
+            <section className="user__profile">
+                <article className="user__info">
+                    <div className="user__img">
+                        <img className="user__avatar" src={userImage} />
+                    </div>
+
+                    <div className="user__data">
+                        <div className="user__names">
+                            <h4 className="user__title">Name</h4>
+                            <p className="user__input">{userName}</p>
+                        </div>
+
+                        <div className="user__ranks">
+                            <h4 className="user__title">Rank</h4>
+                            <p className="user__input">Kindness Master</p>
+                        </div>
+
+                        <div className="user__location">
+                            <h4 className="user__title">Location</h4>
+                            <p className="user__input">Vancouver, BC</p>
+                        </div>
+                    </div>
+                </article>
+
+                <div className="user__info">
+                    <div className="user__about">
+                        <h4 className="user__title">About me</h4>
+                        <p className="user__input">{userDescription}</p>
+                    </div>
+
+                    <div className="user__stats">
+                        <h4 className="user__title"> Stats</h4>
+                        <img className="user__icon" src={heart} />
+                        <p className="user__input">23 kind acts completed!</p>
+                    </div>
+
+                </div>
+            </section>
+
+            {/* <div className="user__profile">
                 <h3 className="user__name">{userName}</h3>
                 <img className="user__img" src={userImage}></img>
             </div>
@@ -27,7 +67,7 @@ function UserProfile({ userImage, userName, userDescription }) {
                         <p>23 acts of kindness completed! Way to go!</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
