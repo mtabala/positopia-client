819a49933797876dd580cb541f776f29e0e68cf8
Masha <mariia.tabala@gmail.com>
2023-04-10 13:05:33 -0700

    create the jsx and styling for Profile Settings and User Side Bar components,connect to server side to get data about the user

--- src/components/ProfileSettings/ProfileSettings.jsx
+++ src/components/ProfileSettings/ProfileSettings.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
+import "./ProfileSettings.scss"
+import { ReactComponent as UserIcon } from "../../assets/icons/user.svg";
 
-export default function ProfileSettings() {
+function ProfileSettings({ name, location, email, image }) {
     return (
-        <p>ProfileSettings</p>
+        <div className="edit">
+            <div className="edit__wrapper">
+                <div className="edit__title">
+                    <span className="edit__title-update">Update Your Account</span>
+                    <span className="edit__title-delete">Delete Account</span>
+                </div>
+
+                <form className="edit__form" >
+                    <label className="edit__label">Profile Picture</label>
+                    <div className="edit__photo">
+                        <img
+                            src={image}
+                            alt="user avatar"
+                        />
+                        <label className="edit__label" htmlFor="fileInput">
+                            <UserIcon className="edit__photo-icon" />
+                        </label>
+                        <input
+                            type="file"
+                            id="fileInput"
+                            style={{ display: "none" }}
+                        />
+                    </div>
+
+                    <div className="edit__inputs-wrapper">
+                        <div className="edit__inputs">
+                            <label className="edit__label">Username</label>
+                            <input className="edit__input"
+                                type="text"
+                                placeholder={name}
+                            />
+                            <label className="edit__label">Email</label>
+                            <input className="edit__input"
+                                type="email"
+                                placeholder={email}
+                            />
+                            <label className="edit__label">Password</label>
+                            <input className="edit__input"
+                                type="password"
+                            />
+                        </div>
+                        <div className="edit__inputs">
+                            <label className="edit__label">About me</label>
+                            <input className="edit__input edit__input--about"
+                                type="text"
+                                placeholder="About Me"
+                            />
+                            <label className="edit__label">Location</label>
+                            <input className="edit__input"
+                                type="text"
+                                placeholder={location}
+                            />
+                        </div>
+                    </div>
+
+                    <button className="edit__btn btn" type="submit">
+                        Update
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
+export default ProfileSettings;
--- src/components/UserSideBar/UserSideBar.jsx
+++ src/components/UserSideBar/UserSideBar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import "./UserSideBar.scss"
 
-export default function UserSideBar() {
+function UserSideBar({ name, rank, location, description, image }) {
+
+
     return (
-        <p>UserSideBar</p>
+        <aside className="sidebar">
+
+            <div className="sidebar__item">
+                <h3 className="sidebar__title">About Me</h3>
+                <img className="sidebar__img" src={image} alt="user" />
+                <p className="sidebar__text">{description}</p>
+            </div>
+
+            <div className="sidebar__item">
+                <h6 className="sidebar__title">Info</h6>
+                <ul className="sidebar__list">
+                    <li className="sidebar__list-item"><span className="sidebar__list-title">Name:</span> {name} </li>
+                    <li className="sidebar__list-item"><span className="sidebar__list-title">Rank:</span> {rank}</li>
+                    <li className="sidebar__list-item"><span className="sidebar__list-title">Location:</span> {location} </li>
+                </ul>
+            </div>
+        </aside>
     )
+
 }
+
+export default UserSideBar;
